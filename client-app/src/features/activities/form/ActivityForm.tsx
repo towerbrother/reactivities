78f6e27d517ad8528d1499e28689e380e2bbe3ac
Giorgio Torre <giorgio.torre@stellenanzeigen.de>
2021-03-14 13:52:24 +0100

    added a options dropdown common component

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -9,6 +9,8 @@ import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import MyTextInput from "./../../../app/common/form/MyTextInput";
 import MyTextArea from "./../../../app/common/form/MyTextArea";
+import MySelectInput from "./../../../app/common/form/MySelectInput";
+import { categoryOptions } from "./../../../app/common/options/CategoryOptions";
 
 export default observer(function ActivityForm() {
   const history = useHistory();
@@ -76,7 +78,11 @@ export default observer(function ActivityForm() {
           <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
             <MyTextInput placeholder="Title" name="title" />
             <MyTextArea placeholder="Description" name="description" rows={3} />
-            <MyTextInput placeholder="Category" name="category" />
+            <MySelectInput
+              placeholder="Category"
+              name="category"
+              options={categoryOptions}
+            />
             <MyTextInput placeholder="Date" name="date" />
             <MyTextInput placeholder="City" name="city" />
             <MyTextInput placeholder="Venue" name="venue" />
