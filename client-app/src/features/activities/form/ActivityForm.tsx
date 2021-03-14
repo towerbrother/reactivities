eafde7fc1fd311e77547b7c52542e71eadd7d636
Giorgio Torre <giorgio.torre@stellenanzeigen.de>
2021-03-14 14:06:20 +0100

    added a common date input and styling

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -10,6 +10,7 @@ import * as Yup from "yup";
 import MyTextInput from "./../../../app/common/form/MyTextInput";
 import MyTextArea from "./../../../app/common/form/MyTextArea";
 import MySelectInput from "./../../../app/common/form/MySelectInput";
+import MyDateInput from "./../../../app/common/form/MyDateInput";
 import { categoryOptions } from "./../../../app/common/options/CategoryOptions";
 
 export default observer(function ActivityForm() {
@@ -83,7 +84,13 @@ export default observer(function ActivityForm() {
               name="category"
               options={categoryOptions}
             />
-            <MyTextInput placeholder="Date" name="date" />
+            <MyDateInput
+              placeholderText="Date"
+              name="date"
+              showTimeSelect
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm aa"
+            />
             <MyTextInput placeholder="City" name="city" />
             <MyTextInput placeholder="Venue" name="venue" />
             <Button
